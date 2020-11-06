208d2de824051023c301dd4baf699a313437cd46
Param Siddharth <contact@paramsid.com>
2020-11-07 01:24:36 +0530

    Web implementation almost done; Annoying SelectPicker bug to fix

--- Makefile
+++
@@ -1,5 +0,0 @@
-pattern:
-	gcc pattern.c -o pattern.out
-
-wasm:
-	emcc -o pattern.js pattern.c -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' -s EXPORTED_FUNCTIONS='["_pattern"]'
---
+++ data/render.js
@@ -0,0 +1,97 @@
+// Available patterns
+var patterns = [
+	'square',
+	'swastika',
+	'triangle',
+	'diamond',
+	'asterisk'
+];
+const DEFAULT_SIZE = 4;
+
+// Validate and return fill
+function getFill() {
+	let fill = $('#fill').val();
+	if (fill.length < 1) {
+		$('#fill').prop('selectedIndex', 0);
+		return $('#fill').val();
+	}
+	if (fill.length > 1)
+		fill = fill[0];
+	return fill;
+}
+
+// Validate and return size
+function getSize() {
+	let size = $('#size').val();
+	try {
+		size = parseInt(size, 10);
+		if (size > 999)
+			size = 999;
+		if (size < 1)
+			size = 1;
+		$('#size').val(size);
+		return size;
+	} catch(e) {
+		$('#size').val(DEFAULT_SIZE);
+		return DEFAULT_SIZE;
+	}
+}
+
+// Validate and return pattern-type
+function getPatternType() {
+	let patternType = $('#patternType').prop('selectedIndex');
+	if (0 <= patternType && patternType < patterns.length)
+		return patterns[patternType];
+	return patterns[0];
+}
+
+// Generate the pattern
+function generatePattern() {
+	let fill = getFill(),
+		size = getSize(),
+		patternType = getPatternType();
+	let patternText = window.pattern(fill, size, patternType);
+	$('#patternOutput').text(patternText);
+	$('#patternOutput')[0].style.height = '';
+	$('#patternOutput')[0].style.height = $('#patternOutput')[0].scrollHeight + 'px';
+}
+
+// Handle WASM module initialization
+Module = {
+	onRuntimeInitialized: () => {
+		window.pattern = Module.cwrap('pattern', 'string', ['string', 'number', 'string']); // Load the wrapper for the C Pattern function
+		// console.log('Runtime initialized');
+
+		// After loading
+		$(function() {
+			// Generate the pattern upon loading
+			generatePattern();
+
+			// Handle changing of parameters
+			$('#fill').on('change', function() {
+				if ($(this).val() === 'Custom...' && $(this).prop('selectedIndex') == patterns.length) {
+					let newFill = prompt('Enter a customized fill: ');
+					if (newFill.length > 0) {
+						$(this).removeClass('selectpicker');
+						$(this).children('option').last().remove();
+						$(this).append($('<option></option>').text(newFill));
+						$(this).append($('<option></option>').text('Custom...'));
+						patterns.push(newFill);
+						$(this).prop('selectedIndex', patterns.length - 1);
+						$(this).selectpicker();
+					}
+				}
+				generatePattern();
+			});
+			$('#size').on('keydown', function(e) { e.preventDefault(); });
+			$('#size').on('input', function() {
+				if (isNaN($('#size').val()))
+					$('#size').val(DEFAULT_SIZE);
+				generatePattern();
+				$('#size').val(getSize());
+			});
+			$('#patternType').on('change', generatePattern);
+		});
+	}
+};
+
